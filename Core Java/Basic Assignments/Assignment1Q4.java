31a32b6d0ed9bb6d59e962532b97d4c7c614b2ae
Yashasvi Pamu <yashasvistark028@gmail.com>
2021-01-29 00:04:26 +0530

    Update Assignment1Q4.java

--- Core Java/Basic Assignments/Assignment1Q4.java
+++ Core Java/Basic Assignments/Assignment1Q4.java
@@ -4,27 +4,21 @@ import java.util.Scanner;
 
 class ResultDeclaration{
     public String declareResults( double subject1Marks, double subject2Marks, double subject3Marks) {
-    	String pass="Passed";
-    	String promoted="promoted";
-    	String fail="failed";
-    	String str="";
-    	if(subject1Marks>60 || subject2Marks>60 || subject3Marks>60 )
-    	{
-    		str=fail;
-    	}
-    	if(subject1Marks+subject2Marks>60 || subject2Marks+subject3Marks>60 || subject3Marks+subject1Marks>60)
-    	{
-    		str=str+"\n"+promoted;
-    	}
-    	if(subject1Marks+subject2Marks+subject3Marks>60)
-    	{
-    		str=str+"\n"+pass;
-    	}
-    	else
-    	{
-    		str=fail;
-    	}
-    	return str;
+    	String str=" ";
+        if((subject1Marks+subject2Marks+subject3Marks)<60||subject1Marks>60||subject2Marks>60||subject3Marks>60)
+        {
+            str="failed";
+        }
+        else if((subject1Marks+subject2Marks>60)||(subject2Marks+subject3Marks>60)||(subject3Marks+subject1Marks>60))
+        {
+            str= "passed\npromoted";
+        }
+        else if ((subject1Marks+subject2Marks+subject3Marks)>60)
+        {
+            str="passed";
+        }
+
+        return str;
     	
     }
 }
@@ -39,3 +33,4 @@ public class Assignment1Q4 {
     	System.out.println(result.declareResults(sub1, sub2, sub3));
     }
 }
+
