963ed7d90b474371d8c290daeed05b246e48ed8c
Yashasvi Pamu <yashasvistark028@gmail.com>
2021-01-28 20:06:42 +0530

    Update Assignment4Q3.java

--- Core Java/Lambda Expression Assignment/Assignment4Q3.java
+++ Core Java/Lambda Expression Assignment/Assignment4Q3.java
@@ -7,7 +7,12 @@ import java.util.function.Supplier;
 
 public class Assignment4Q3 {
 	static Product product=new Product("Brush");
-    
+	
+	static int modifyValue(int a, Function<Integer, Integer> b)
+    { 
+      return b.apply(a);
+    }
+	
 	static void modifyValue()
     { 
     	Consumer<Product> updateName = a -> a.setName("ToothBrush");
@@ -32,13 +37,14 @@ public class Assignment4Q3 {
     
     static void display() 
     {
+        
     	System.out.println(product.getName()); 
     	
     	Supplier<Integer> supplier = () -> new Integer((int) (Math.random() * 1000D));
     	System.out.println(supplier.get());
     	
-    	Function<Integer, Integer> adder = (value) -> value * 3;
-    	Integer resultLambda = adder.apply(8);
+    	Function<Integer, Integer> multiply = (value) -> value * 3;
+    	Integer resultLambda = multiply.apply(8);
     	System.out.println("resultLambda = " + resultLambda);
     	
     	Predicate<String> predicate = (value) -> value.length()>5;
