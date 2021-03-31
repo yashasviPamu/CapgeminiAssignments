66498639b5cecf61d82bfb04555d428217fe4005
Yashasvi Pamu <yashasvistark028@gmail.com>
2021-03-31 14:32:23 +0530

    Create Q1.ts

---
+++ ES6 & Typescript/Assignment 2/Q1.ts
@@ -0,0 +1,22 @@
+class fibonacciClass {
+  private previousNo: number;
+  private currentNo: number;
+  constructor(curr: number) {
+    this.previousNo = fibo(curr - 1);
+    this.currentNo = curr;
+  }
+  next() {
+    return fibo(this.currentNo + 1);
+  }
+}
+var fibo = function (n: number) {
+  var a = 0, b = 1, f = 1;
+  for (var i = 2; i <= n; i++) {
+    f = a + b;
+    a = b;
+    b = f;
+  }
+  return f;
+};
+let fibonacciClass = new fibonacciClass(6);
+console.log(fibonacciClass.next());
