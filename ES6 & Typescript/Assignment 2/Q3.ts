f38fe8e197cfebf1945a1d856200b59e9485893e
Yashasvi Pamu <yashasvistark028@gmail.com>
2021-03-31 15:22:51 +0530

    Create Q3.ts

---
+++ ES6 & Typescript/Assignment 2/Q3.ts
@@ -0,0 +1,39 @@
+  
+let lowNumber = 10;
+console.log("Armstrong Numbers:");
+function armstong() {
+  for (let i = lowNumber; i > 0; i++) {
+    let numberOfDigits = i.toString().length;
+
+    let sum = 0;
+
+    let temp = i.toString();
+
+    for (let char of temp) {
+      sum += Math.pow(parseInt(char), numberOfDigits);
+    }
+    if (sum == i) {
+      console.log(i);
+      lowNumber = i;
+      break;
+    }
+  }
+}
+function getNextArmstrong() {
+  lowNumber++;
+  armstong();
+}
+
+function reset() {
+  lowNumber = 9;
+}
+
+armstong();
+getNextArmstrong();
+getNextArmstrong();
+getNextArmstrong();
+reset();
+armstong();
+getNextArmstrong();
+getNextArmstrong();
+getNextArmstrong();
