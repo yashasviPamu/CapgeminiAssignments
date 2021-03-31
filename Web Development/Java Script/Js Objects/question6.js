607b2ad1856d32da7eec108b41e2f901333bb2d7
Yashasvi Pamu <yashasvistark028@gmail.com>
2021-03-31 09:44:26 +0530

    Create question6.js

---
+++ Web Development/Java Script/Js Objects/question6.js
@@ -0,0 +1,6 @@
+var firstName="Yashasvi";
+var lastName="Pamu";
+var fullName=`${firstName} ${lastName}`;
+console.log(eval(new String(`${lastName},${firstName}`)));
+console.log(eval(new String(fullName)));
+console.log(fullName);
